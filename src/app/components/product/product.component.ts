189ce7209660fd0ae90a7554558f58c457183771
Ögeday Karadal <ogedaykaradal@gmail.com>
2022-07-05 13:11:25 +0300

    5 July 2022 (4)

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,40 +13,43 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  filterText="";
+  filterText = '';
 
-  constructor(private productService:ProductService,
-    private activatedRoute:ActivatedRoute, private toastrService:ToastrService) {}
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
-      this.activatedRoute.params.subscribe(params=>{
-        if(params["categoryId"])
-        {
-          this.getProductsByCategory(params["categoryId"])
-        }
-        else
-        {
-          this.getProducts();
-        }
-      })
-
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
+      }
+    });
   }
 
   getProducts() {
     this.productService.getProducts().subscribe((response) => {
-      this.products = response.data
+      this.products = response.data;
       this.dataLoaded = true;
     });
   }
 
-  getProductsByCategory(categoryId:number) {
-    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
-      this.products = response.data
-      this.dataLoaded = true;
-    });
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
   }
 
-  addToCart(product:Product){
-    this.toastrService.success(product.productName,"Added to Cart")
+  addToCart(product: Product) {
+    this.toastrService.success(product.productName, 'Added to Cart');
+    this.cartService.addToCart(product);
   }
 }
